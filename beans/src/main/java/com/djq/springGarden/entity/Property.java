a7685e2173aff80845f48b541456aef6659b9faf
爱吃小橘子 <code@acxjz.com>
2022-02-12 15:23:01 +0800

    去除无用的引用

--- beans/src/main/java/com/djq/springGarden/entity/Property.java
+++ beans/src/main/java/com/djq/springGarden/entity/Property.java
@@ -1,13 +1,10 @@
 package com.djq.springGarden.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.Date;
-import java.math.BigDecimal;
 import javax.persistence.*;
 
 /**
